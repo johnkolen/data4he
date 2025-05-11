8d20affd8dfbf3b0b05a223adb2825534f6db8bd
John Kolen <johnfkolen@gmail.com>
2025-05-10 18:09:44 -0600

    junk

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_06_022202) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "people", force: :cascade do |t|
-    t.string "first_name_h"
-    t.string "last_name_h"
-    t.date "date_of_birth_h"
-    t.string "ssn_h"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
